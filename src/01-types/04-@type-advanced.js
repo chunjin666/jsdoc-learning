33729779abdb7338057bfd99a3d59c9a5b72d1da
chunjin.xu <chunjine@163.com>
2022-07-02 23:41:02 +0800

    feat(casts): 类型投影使用说明更新，补全 as const 这一短板

--- src/01-types/04-@type-advanced.js
+++ src/01-types/04-@type-advanced.js
@@ -1,5 +1,6 @@
 // ----------------------------------------------------- Casts （类型投影？类型转换?）
 // 在小括号括起来的表达式前面添加类型注释
+// !!! TS 版本需要大于等于 4.5，且这个特性在 TS 环境下不生效
 
 /**
  * @type {number | string}
@@ -12,6 +13,13 @@ var typeAssertedNumber = /** @type {number} */ (numberOrString);
 let one = /** @type {const} */ (1);
 // @ts-ignore
 let oneStr = /** @type {const} */ ('1');
+
+let deepConst = /** @type {const} */ ({
+  layer1key1: 1,
+  layer1key2: {
+    layer2key1: '11'
+  }
+})
 
 // ----------------------------------------------------- typeof
 // 当某个值的类型比较复杂写起来比较麻烦时可以用`typeof`来获取其类型。
